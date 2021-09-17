7d0b24aeb8f5d1e123a9ff2d8f71cc069743926b
Dmitrii <zeionara@niuitmo.ru>
2021-09-17 23:41:42 +0300

    Added BARGE_OPTIONS env variable for specifying the list of supported arguments

--- parse.sh
+++ parse.sh
@@ -4,9 +4,13 @@ source $BARGE_ROOT/string-utils.sh
 source $BARGE_ROOT/handler-generators.sh
 source $BARGE_ROOT/array-utils.sh
 
+if [ -z "$BARGE_OPTIONS" ]; then
+    echo "BARGE_OPTIONS env variable is not specified, please declare the list of supported arguments in format: '<foo-arg-short-name>|<foo-arg-full-name> <baz-arg-short-name>|<bar-arg-full-name>'"
+    exit 1
+fi
+
 # Generate handlers from the option list
-option_list="f|foo-bar q|baz-qux c|corge-grault"
-split_string "$option_list"
+split_string "$BARGE_OPTIONS"
 
 # Save the result returned from the splitting function into another array to not to lose it after the next split operation
 copy_array __items parsed_options
